ff011f2d5901b84760d3327a70cca662d5cbc4c5
Shen Xinwei <shenxwtsinghua@126.com>
2023-12-27 18:33:35 +0800

    Update TEP.m
    完成了一个基本的6节点11线路输电网规划算例。供大家参考

--- TEP.m
+++ TEP.m
@@ -1,31 +1,35 @@
+%% 一个简单的输电网扩展规划算例示意
+% 基于matpower中的6节点网络，case6ww.m。
 clear all
 close all
 clc
+define_constants; %打开这个函数可明确mpc的各个矩阵包含的信息，同时参见[1]
+% [1]Appendix B Data File Format, MATPWER User s Manual Version 7.1
 D=runpf('case6ww');
 mpc=case6ww;
-
-
 
 %% ***********Parameters **********
 N=6; % number of load nodes
 L=11; % number of all lines
 Sbase=mpc.baseMVA;  % unit:VA
-pd = mpc.bus(:,3)/Sbase;
-g_N_thermal = mpc.gen(:,9)/Sbase; %火电机组容量
-p_max = 1.5;%线路传输功率上限
-X = mpc.branch(:,4);
-c_lines = X*100; %用线路电抗代表线路长度，得到线路建设成本
+pd = mpc.bus(:,PD)/Sbase;
+g_max = mpc.gen(:,PMAX)/Sbase; %火电机组容量
+p_max = 1.5; %线路传输功率上限
+xb = mpc.branch(:,BR_X); 
+c_lines = xb*100;  %用线路电抗代表线路长度，得到线路建设成本c_lines
 
-I = mpc.branch(:,1);
-J = mpc.branch(:,2);
-In = myincidence(I,J); 
-l_E=[1,2,4,6,9]; % 已建设线路
-l_c=setdiff([1:L],l_E);
+I = mpc.branch(:,F_BUS);
+J = mpc.branch(:,T_BUS);
+[Ainc] = makeIncidence(mpc); % branch-node incidence matrix
+In=Ainc'; % node-branch incidence matrix, but with all lines closed
+% In = myincidence(I,J); 
+l_E=[1,2,4,6]; % 已建设线路
+l_c=setdiff([1:L],l_E); %待建设线路选项
 G = digraph(I,J);
 figure;
 h = plot(G, 'Layout', 'force', 'EdgeColor', 'k', 'NodeColor', 'b', 'MarkerSize', 8);
-highlight(h,I(l_E),J(l_E),'LineWidth',3,'EdgeColor','k');
-highlight(h,I(l_c),J(l_c),'LineStyle','-.','LineWidth',1,'EdgeColor','b');
+highlight(h,I(l_E),J(l_E),'LineWidth',3,'EdgeColor','k'); %画出已建设线路
+highlight(h,I(l_c),J(l_c),'LineStyle','-.','LineWidth',1,'EdgeColor','b'); %画出待建设线路选项
 
 M = 1e5;
 
@@ -40,8 +44,8 @@ x = binvar(L,1);
 g = sdpvar(N,1);
 
 %% ***********Constraints*************
-c2=mpc.gencost(:,5);
-c1=mpc.gencost(:,6);
+c2=mpc.gencost(:,5); %发电成本函数二次项系数
+c1=mpc.gencost(:,6); %发电成本函数一次项系数
 Obj = sum(c_lines.*x)+sum(c2.*g(1:3).*2+c1.*g(1:3))+M*sum(pd_shed);
 Cons = [];
 
@@ -52,9 +56,9 @@ Cons=[x(l_E)==1];
 Cons_DC=[];
 Cons_DC=[
     % In*p==g-d,In'*theta==p.*X,theta(1)==0
-    -(1-x)*M <= In'*theta - p.* X <= (1-x)*M,
+    -(1-x)*M <= In'*theta - p.* xb <= (1-x)*M,
     In*p==g-(pd-pd_shed);
-    theta(2)==0,
+    theta(1)==0,
     -x.*p_max <= p <=x.*p_max;
     pd_shed>=0;
 ];
@@ -62,7 +66,7 @@ Cons_DC=[
 Cons=[Cons, Cons_DC];
 
 %机组发电功率约束
-Cons=[Cons, 0 <= g(1:3,:) <= g_N_thermal, g(4:6,:) == 0];
+Cons=[Cons, 0 <= g(1:3,:) <= g_max, g(4:6,:) == 0];
 
 %% Solve the TEP problem
 ops=sdpsettings('verbose',2,'solver','gurobi');
@@ -77,8 +81,10 @@ s_g = value(g)
 figure
 h2 = plot(G, 'Layout', 'force', 'EdgeColor', 'k', 'NodeColor', 'b', 'MarkerSize', 8);
 h2.EdgeLabel = s_p;
-highlight(h2,I(find(round(s_x)==1)),J(find(round(s_x)==1)),'LineWidth',3,'EdgeColor','k');
-highlight(h2,I(find(round(s_x)==0)),J(find(round(s_x)==0)),'LineStyle','--','LineWidth',1,'EdgeColor','b');
+l_new=setdiff(find(round(s_x)==1),l_E); %新建的线路
+highlight(h2,I(l_E),J(l_E),'LineWidth',3,'EdgeColor','k'); %已建设线路
+highlight(h2,I(find(round(s_x)==0)),J(find(round(s_x)==0)),'LineStyle','--','LineWidth',1,'EdgeColor','b'); %未建设线路
+highlight(h2,I(l_new),J(l_new),'LineStyle','--','LineWidth',3,'EdgeColor','g'); %新建设线路
 % h.NodeLabel = s_g;
 title('规划结果');
 
