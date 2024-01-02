5a050131cdd9555e7e346900ff504bd0608574bd
YUE YU <1257197760@qq.com>
2024-01-03 03:14:00 +0800

    Update TEP.m

--- TEP.m
+++ TEP.m
@@ -12,10 +12,10 @@ mpc=case6ww;
 N=6; % number of load nodes
 L=11; % number of all lines
 Sbase=mpc.baseMVA;  % unit:VA
-pd = mpc.bus(:,PD)/Sbase;
+pd = mpc.bus(:,PD)/Sbase; %负荷需求标幺值
 g_max = mpc.gen(:,PMAX)/Sbase; %火电机组容量
-p_max = 1.5; %线路传输功率上限
-xb = mpc.branch(:,BR_X); 
+p_max = mpc.branch(:,RATE_A)/Sbase; %线路传输功率上限
+xb = mpc.branch(:,BR_X); %线路电抗
 c_lines = xb*100;  %用线路电抗代表线路长度，得到线路建设成本c_lines
 
 I = mpc.branch(:,F_BUS);
@@ -23,14 +23,33 @@ J = mpc.branch(:,T_BUS);
 [Ainc] = makeIncidence(mpc); % branch-node incidence matrix
 In=Ainc'; % node-branch incidence matrix, but with all lines closed
 % In = myincidence(I,J); 
-l_E=[1,2,4,6]; % 已建设线路
-l_c=setdiff([1:L],l_E); %待建设线路选项
+l_status = zeros(1, L);
+l_E = [2,3,6,8,9]; % 已建设线路
+l_c = setdiff([1:L],l_E); %待建设线路选项
+l_status(l_E)= 1;
+
+%% 计算当前线路连接状态下的潮流并画图
+mpc.branch(:, 11) = l_status;
+result = runpf(mpc);
+
+% 潮流结果
+fprintf('线路潮流:\n');
+disp(result.branch(:, [1, 2, 14]));
+% 绘图
 G = digraph(I,J);
 figure;
 h = plot(G, 'Layout', 'force', 'EdgeColor', 'k', 'NodeColor', 'b', 'MarkerSize', 8);
+h.EdgeLabel = result.branch(:, 14)/Sbase;
 highlight(h,I(l_E),J(l_E),'LineWidth',3,'EdgeColor','k'); %画出已建设线路
 highlight(h,I(l_c),J(l_c),'LineStyle','-.','LineWidth',1,'EdgeColor','b'); %画出待建设线路选项
+title('初始线路');
 
+% 在节点上标记发电机出力信息
+for i = 1:3
+    label_str = sprintf('机组出力：%.2f', result.gen(i, 2)/Sbase);    
+    text(h.XData(i)+0.2, h.YData(i)-0.1, label_str, ...
+        'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle', 'Color', 'b', 'FontSize', 8);
+end
 M = 1e5;
 
 %% Nodal Y Matrix→B matrix
@@ -88,3 +107,10 @@ highlight(h2,I(l_new),J(l_new),'LineStyle','--','LineWidth',3,'EdgeColor','g'); 
 % h.NodeLabel = s_g;
 title('规划结果');
 
+
+% 在节点上标记发电机出力信息
+for i = 1:3
+    label_str = sprintf('机组出力：%.2f',s_g(i));    
+    text(h2.XData(i)+0.2, h2.YData(i)-0.1, label_str, ...
+        'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle', 'Color', 'b', 'FontSize', 8);
+end
