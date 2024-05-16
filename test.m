08fb7ab4e771ed98d2c3dd246700584ca7ab10fc
Shen Xinwei <shenxwtsinghua@126.com>
2024-05-16 16:48:00 +0800

    Update test.m

--- test.m
+++ test.m
@@ -9,15 +9,13 @@ D=runpf('case6ww');
 mpc=case6ww;
 
 %% ***********Parameters **********
-Hours = 24; 
+Hours = 24;
 N = 6; % number of load nodes
 L = 11; % number of all lines
 K = 4; % number of coal generation types
 Sbase = mpc.baseMVA;  % unit:VA
-
-g_max = mpc.gen(:,PMAX)/Sbase; %火电机组容量
-g_max_all = [0.6,1,1.2,2];
-g_min_all = [0.1,0.1,0.1,0.1];
+g_max_all = [30,60,100,120]/Sbase;
+g_min_all = [0.7,0.7,0.55,0.55] .* g_max_all;
 x_coal_max = [4,14,1,3];
 p_max = mpc.branch(:,RATE_A)/Sbase; %线路传输功率上限
 xb = mpc.branch(:,BR_X); %线路电抗
@@ -34,7 +32,7 @@ c_gen = [c0;280];
 I = mpc.branch(:,F_BUS);
 J = mpc.branch(:,T_BUS);
 [Ainc] = makeIncidence(mpc); % branch-node incidence matrix
-In=Ainc'; % node-branch incidence matrix, but with all lines closed 
+In=Ainc'; % node-branch incidence matrix, but with all lines closed
 %sum_N_g = zeros(N, T);
 Cu_NL = [2 3 4 5]; % No-load cost of unit
 Cup = [5 10 5 5]; % Startup cost of unit
@@ -73,12 +71,12 @@ title('初始线路');
 
 % 在节点上标记发电机出力信息
 for i = 1:3
-    label_str = sprintf('机组出力：%.2f', result.gen(i, 2)/Sbase);    
+    label_str = sprintf('机组出力：%.2f', result.gen(i, 2)/Sbase);
     text(h.XData(i)+0.2, h.YData(i)-0.1, label_str, ...
         'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle', 'Color', 'b', 'FontSize', 8);
 end
 for i= 4:6
-    label_str = sprintf('负荷需求：%.2f',pd(i)); 
+    label_str = sprintf('负荷需求：%.2f',pd(i));
     text(h.XData(i)-0.4, h.YData(i)+0.2, label_str, ...
         'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle', 'Color', 'b', 'FontSize', 8);
 end
@@ -109,7 +107,6 @@ g_coal_3 = sdpvar(N,x_coal_max(3),Hours,'full');
 g_coal_4 = sdpvar(N,x_coal_max(4),Hours,'full');
 sum_N_g = sdpvar(N,1,Hours);%6个节点的机组输出功率
 sum_type_g = sdpvar(N,length(x_coal_max),Hours,'full');%四种类型一共发了多少
-%g = sdpvar(N,K,Hours,'full');%节点N的第K台机组在t时段的输出功率
 %x_gen_gas = binvar(3,5);
 %x_gen_pws = binvar(3,2);
 %x_gen_nuc = binvar(3,1);
@@ -127,57 +124,59 @@ Obj = 0;
 Obj_u = 0;
 Obj_up = 0;
 Obj_down = 0;
-Cons = [Cons,sum_type_g(:,1,:) == sum(g_coal_1,2)]; 
+Obj_ope = sdpvar(1,Hours);
+Cons = [Cons,sum_type_g(:,1,:) == sum(g_coal_1,2)];
 Cons = [Cons,sum_type_g(:,2,:) == sum(g_coal_2,2)];
 Cons = [Cons,sum_type_g(:,3,:) == sum(g_coal_3,2)];
 Cons = [Cons,sum_type_g(:,4,:) == sum(g_coal_4,2)];
 for t = 1:Hours
-    Obj_ope = M * sum(pd_shed(:,t)) + sum(c2.*g_exist(1:3,t).^2 + c1.*g_exist(1:3,t));%原有机组发电成本
+    Obj_ope(1,t) = M * sum(pd_shed(:,t)) + sum(c2.*g_exist(1:3,t).^2 + c1.*g_exist(1:3,t));%原有机组发电成本
     %新增机组发电成本
-    for i = 1:N 
-    Obj_ope = Obj_ope + sum(c2_all.*sum_type_g(i,:,t).^2 + c1_all.*sum_type_g(i,:,t));
-    %Obj_u = Obj_u + sum(Cu_NL.*u(i,:,t));机组组合部分未改
-    %Obj_up = Obj_up + sum(Cup.*v(i,:,t));
-    %Obj_down = Obj_down + sum(Cdown.*w(i,:,t));
+    for i = 1:N
+        Obj_ope(1,t) = Obj_ope(1,t) + sum(c2_all.*sum_type_g(i,:,t).^2 + c1_all.*sum_type_g(i,:,t));
+        %Obj_u = Obj_u + sum(Cu_NL.*u(i,:,t));机组组合部分未改
+        %Obj_up = Obj_up + sum(Cup.*v(i,:,t));
+        %Obj_down = Obj_down + sum(Cdown.*w(i,:,t));
     end
-Obj = Obj + Obj_inv + Obj_ope + Obj_u + Obj_up + Obj_down;
-%% Cons1: 直流潮流约束+TEP
-Cons=[Cons,x_lines(l_E) == 1];%已建设线路
-Cons_DC=[Cons,-(1 - x_lines)*M <= In'*theta(:,t) - p(:,t).* xb];
-Cons_DC=[Cons_DC,In'*theta(:,t) - p(:,t).* xb <= (1 - x_lines) * M];
-Cons_DC=[Cons_DC,sum_N_g == sum(g_coal_1,2) + sum(g_coal_2,2) + sum(g_coal_3,2) + sum(g_coal_4,2)];
-Cons_DC=[Cons_DC,In * p(:,t) == sum_N_g(:,:,t) + g_exist(:,t) - (P_load(:,t) - pd_shed(:,t)) ];  
-Cons_DC=[Cons_DC,theta(1,t) == 0];  
-Cons_DC=[Cons_DC,- x_lines .* p_max <= p(:,t)];  
-Cons_DC=[Cons_DC,p(:,t) <= x_lines .* p_max];  
-Cons_DC=[Cons_DC,pd_shed(:,t) >= 0];
-Cons=[Cons_DC];
-%% Cons2: 机组发电功率约束
-% 原有机组
-Cons=[Cons, g_exist((2:6),t) == 0];%初始条件：节点1有一台机组
-Cons=[Cons,0 <= g_exist(1,t); g_exist(1,t)<= g_max(1)];
-% 新增机组
-for i=1:3
+    %% Cons1: 直流潮流约束+TEP
+    Cons=[Cons,x_lines(l_E) == 1];%已建设线路
+    Cons_DC=[Cons,-(1 - x_lines)*M <= In'*theta(:,t) - p(:,t).* xb];
+    Cons_DC=[Cons_DC,In'*theta(:,t) - p(:,t).* xb <= (1 - x_lines) * M];
+    Cons_DC=[Cons_DC,sum_N_g == sum(g_coal_1,2) + sum(g_coal_2,2) + sum(g_coal_3,2) + sum(g_coal_4,2)];
+    Cons_DC=[Cons_DC,In * p(:,t) == sum_N_g(:,:,t) + g_exist(:,t) - (P_load(:,t) - pd_shed(:,t)) ];
+    Cons_DC=[Cons_DC,theta(1,t) == 0];
+    Cons_DC=[Cons_DC,- x_lines .* p_max <= p(:,t)];
+    Cons_DC=[Cons_DC,p(:,t) <= x_lines .* p_max];
+    Cons_DC=[Cons_DC,pd_shed(:,t) >= 0];
+    Cons=[Cons_DC];
+    %% Cons2: 机组发电功率约束
+    % 原有机组
+    Cons=[Cons, g_exist((2:6),t) == 0];%初始条件：节点1有一台机组
+    Cons=[Cons,g_min_all(1) <= g_exist(1,t); g_exist(1,t)<= g_max_all(1)];
+    % 新增机组
+    for i=1:3
         %Cons = [Cons, u(i,k,t) <= x_gen_coal(i,k)];%运行的机组和投建机组的关系
         Cons=[Cons, x_gen_coal_1(i,:).*g_min_all(1) <= g_coal_1(i,:,t) <= x_gen_coal_1(i,:).*g_max_all(1)];%机组发电功率上限
         Cons=[Cons, x_gen_coal_2(i,:).*g_min_all(2) <= g_coal_2(i,:,t) <= x_gen_coal_2(i,:).*g_max_all(2)];
         Cons=[Cons, x_gen_coal_3(i,:).*g_min_all(3) <= g_coal_3(i,:,t) <= x_gen_coal_3(i,:).*g_max_all(3)];
         Cons=[Cons, x_gen_coal_4(i,:).*g_min_all(4) <= g_coal_4(i,:,t) <= x_gen_coal_4(i,:).*g_max_all(4)];
+    end
+    % 只有前三个节点可以建设机组
+    Cons=[Cons, g_coal_1((4:6),:,t) == 0];
+    Cons=[Cons, g_coal_2((4:6),:,t) == 0];
+    Cons=[Cons, g_coal_3((4:6),:,t) == 0];
+    Cons=[Cons, g_coal_4((4:6),:,t) == 0];
 end
-Cons=[Cons, g_coal_1((4:6),:,t) == 0];
-Cons=[Cons, g_coal_2((4:6),:,t) == 0];
-Cons=[Cons, g_coal_3((4:6),:,t) == 0];
-Cons=[Cons, g_coal_4((4:6),:,t) == 0];
 
-end
+Obj = Obj_inv + sum(Obj_ope); %+ Obj_u + Obj_up + Obj_down;
 %% Cons3: 机组启停时间约束
 for k=1:K
     for n = 1:3
         for t = TUg(k):Hours
-        Cons = [Cons,sum(v(n,k,(t-TUg(k)+1):t),3)<=u(n,k,t)];
+            Cons = [Cons,sum(v(n,k,(t-TUg(k)+1):t),3)<=u(n,k,t)];
         end
         for t=TDg(k):Hours
-        Cons = [Cons,sum(w(n,k,(t-TDg(k)+1):t),3)<=1-u(n,k,t)];
+            Cons = [Cons,sum(w(n,k,(t-TDg(k)+1):t),3)<=1-u(n,k,t)];
         end
     end
 end
@@ -208,10 +207,16 @@ s_g_coal_1 = value(g_coal_1);
 s_g_coal_2 = value(g_coal_2);
 s_g_coal_3 = value(g_coal_3);
 s_g_coal_4 = value(g_coal_4);
+s_sum_N_g = value(sum_N_g);
+s_sum_type_g = value(sum_type_g);
 s_u = value(u);
 s_v = value(v);
 s_w = value(w);
-s_g_exist = value(g_exist) ;
+s_g_exist = value(g_exist);
+s_Obj = value(Obj);
+s_Obj_inv = value(Obj_inv);
+s_Obj_ope = value(Obj_ope);
+
 figure
 h2 = plot(G, 'Layout', 'force', 'EdgeColor', 'k', 'NodeColor', 'b', 'MarkerSize', 8);
 l_new=setdiff(find(round(s_x_lines)==1),l_E); %新建的线路
@@ -224,10 +229,16 @@ for t = 1:Hours
     Obj_ope = M*sum(pd_shed(:,t))+sum(c2.*g_exist(1:3,t).*2+c1.*g_exist(1:3,t));%原有机组发电成本
     %新增机组发电成本
     for i=1:3
-    Obj_ope= Obj_ope+sum(c2_all .* sum_type_g(i,:,t).*2+c1_all .* sum_type_g(i,:,t));
-    Obj_u=Obj_u+sum(Cu_NL.*u(i,:,t));
-    Obj_up=Obj_up+sum(Cup.*v(i,:,t));
-    Obj_down=Obj_down+sum(Cdown.*w(i,:,t));
+        Obj_ope= Obj_ope+sum(c2_all .* sum_type_g(i,:,t).*2+c1_all .* sum_type_g(i,:,t));
+        Obj_u=Obj_u+sum(Cu_NL.*u(i,:,t));
+        Obj_up=Obj_up+sum(Cup.*v(i,:,t));
+        Obj_down=Obj_down+sum(Cdown.*w(i,:,t));
     end
-fprintf('时段 %d 运行成本: %.2f\n', t, value(Obj_ope)); 
+    fprintf('时段 %d 运行成本: %.2f\n', t, value(Obj_ope));
 end
+
+%% 画运行结果图，输出不同时间段的运行成本
+figure;
+plot(1:Hours, s_Obj_ope, 'b', 'LineWidth', 1.5);
+xlabel('时段/h');
+ylabel('运行成本/万元');
